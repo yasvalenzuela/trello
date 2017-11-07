1f209ea32c8d3a17cabd9341a9d9bbd9861b5534
yasvalenzuela <yasna.valenzuela91@gmail.com>
2017-11-07 16:48:14 -0300

    trello version 0.0.1 a medias

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+var listita = document.getElementById("List");
+listita.addEventListener("click", ocultar);
+
+function ocultar(){
+	document.getElementById("lista").style.display = "none";
+	var newBoard = document.getElementById("formularioOculto");
+	formularioOculto.style.display = "block";
+
+}
+
+

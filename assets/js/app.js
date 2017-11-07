2e4bbed4dc1c80f3ee01ab2cb8360d7c6570c0a2
yasvalenzuela <yasna.valenzuela91@gmail.com>
2017-11-07 17:23:48 -0300

    trello version 0.0.1 completo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
+//para que el formulario comience oculto
+var oculto = document.getElementById('formularioOculto').style.display='none';
+
+//se crea el evento click 
 var listita = document.getElementById("List");
 listita.addEventListener("click", ocultar);
-
+//se crea la funcion con la cual se ocultara el añadir comentario al hacer click y se desplegara el formulario
 function ocultar(){
 	document.getElementById("lista").style.display = "none";
 	var newBoard = document.getElementById("formularioOculto");

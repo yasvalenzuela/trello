ab2d7584422f886506285b5685ee1f7eebb59778
yasvalenzuela <yasna.valenzuela91@gmail.com>
2017-11-09 17:15:09 -0300

    trello version 0.0.2 a medias

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,14 +2,45 @@
 var oculto = document.getElementById('formularioOculto').style.display='none';
 
 //se crea el evento click 
-var listita = document.getElementById("List");
-listita.addEventListener("click", ocultar);
+var listita = document.getElementById('List');
+listita.addEventListener('click', function (){
 //se crea la funcion con la cual se ocultara el añadir comentario al hacer click y se desplegara el formulario
-function ocultar(){
-	document.getElementById("lista").style.display = "none";
-	var newBoard = document.getElementById("formularioOculto");
-	formularioOculto.style.display = "block";
+	document.getElementById("lista").style.display = 'none';
+	var formularioOculto = document.getElementById('formularioOculto');
+	formularioOculto.style.display = 'inline-block';
+});
 
-}
+var guardar = document.getElementById('guardar');
+guardar.addEventListener('click', function(){
+	//texto ingresado
+	var guardado = document.getElementById('listaOculta').value;
+	//limpiar el textarea
+	document.getElementById('listaOculta').value = '';
+	//contenedor que esta en el html
+	var contenedorLista = document.getElementById('contenedorLista');
+	//el div que contiene cada lista
+	var nuevaLista = document.createElement('div');
+	nuevaLista.style.display='inline-block';
+
+	//creo un nodo tipo texto	
+	var textoNombreLista = document.createTextNode(guardado);
+	//creo un parrafo
+	var elementoContenedor = document.createElement('p');
+	elementoContenedor.appendChild(textoNombreLista);
+	nuevaLista.appendChild(elementoContenedor);
+
+	contenedorLista.appendChild(nuevaLista);
+
+	//creo un enlace
+	var enlace = document.createElement('a');
+	//creo un nodo texto que diga añadir tarea
+	var agregar = document.createTextNode('Añadir tarea');
+
+
+	enlace.appendChild(agregar);
+	nuevaLista.appendChild(enlace);
+
+})
+
 
 

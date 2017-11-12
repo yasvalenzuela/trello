1c3640eda242729ededd6a86e586722bd466fce2
yasvalenzuela <yasna.valenzuela91@gmail.com>
2017-11-12 12:04:17 -0300

    version 0.04 y version 0.05 completas, falta arreglar unos detalles

--- assets/js/app.js
+++ assets/js/app.js
@@ -47,6 +47,7 @@ guardar.addEventListener('click', function(){
 	nuevaLista.appendChild(elementoEnlace);
 	contenedorLista.appendChild(nuevaLista);
 
+	//evento de agregar tarea
 	enlace.addEventListener('click', function(){
 		enlace.style.display='none';
 		var contenedorform = document.getElementById('contenedorForm');
@@ -59,6 +60,7 @@ guardar.addEventListener('click', function(){
 		var textarea = document.createElement('textarea');
 		//div que contiene el button y el enlace
 		var button = document.createElement('button');
+		//agrego atributos a button
 		button.setAttribute('type', 'button');
 		button.setAttribute('name', 'añadir');
 		button.setAttribute('id', 'añadir');
@@ -69,6 +71,7 @@ guardar.addEventListener('click', function(){
 		icono.setAttribute('class', 'fa fa-times');
 		icono.setAttribute('aria-hidden', 'true');
 		//class="fa fa-times" aria-hidden="true"
+		//añado nodos al html
 		button.appendChild(buttonAñadir);
 		form.appendChild(textarea);
 		form.appendChild(button);
@@ -76,12 +79,64 @@ guardar.addEventListener('click', function(){
 		contenedorGeneral.appendChild(form);
 		contenedorForm.appendChild(contenedorGeneral);
 
+		textarea.focus();
+
+
+		//evento del boton añadir
+		button.addEventListener('click', function(){
+			//texto ingresado
+			var text = textarea.value;
+			//limpiar el textarea
+			textarea.value = '';
+			textarea.focus();
+			//div contenedor general
+			var contenedorTarea = document.createElement('div');
+			contenedorTarea.setAttribute('id', 'contenedorTarea');
+			contenedorTarea.style.display='block';
+			//creo un div que contenga lo escrito en textarea
+			var textoTextarea = document.createElement('div');
+			textoTextarea.setAttribute('id', 'textoTextarea');
+			textoTextarea.style.display='inline-block';
+			//nodo de texto
+			var tarea = document.createTextNode(text);
+			//creo elemento que contenga nodo de texto
+			var parrafoTarea = document.createElement('p');
+			
+			//añadiendo nodos a html
+			parrafoTarea.appendChild(tarea);
+			textoTextarea.appendChild(parrafoTarea);
+			contenedorTarea.appendChild(textoTextarea);
+			//contenedorForm.appendChild(contenedorTarea);
+
+			contenedorForm.insertBefore(contenedorTarea, contenedorGeneral);
+
+
+		})
 	})
 	
 
 });
 
-
+	//contenedor que esta en el html
+	//var contenedorLista = document.getElementById('contenedorLista');
+	//creo el div que contiene cada lista
+	//var nuevaLista = document.createElement('div');
+	//nuevaLista.setAttribute('id', 'nuevaLista');
+	//div que contiene elemento p
+	//var nuevoElemento = document.createElement('div');
+	//nuevoElemento.setAttribute('id', 'nuevoElemento');
+	//div que contiene elemento enlace
+	//var elementoEnlace = document.createElement('div');
+	//elementoEnlace.setAttribute('id', 'elementoEnlace');
+	//creo un nodo tipo texto	
+	//var textoNombreLista = document.createTextNode(guardado);
+	//creo un parrafo
+	//var elementoContenedor = document.createElement('p');
+	//creo un nodo texto que diga añadir tarea
+	//var agregar = document.createTextNode('Añadir tarea');
+	//creo un enlace
+	//var enlace = document.createElement('a');
+	//enlace.setAttribute('href', '#');
 
 
 
